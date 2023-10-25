3d16d25b1342c352b76e3025a817bfed1677f97d
Roberta Drayer <robertadrayer@gmail.com>
2023-10-24 20:51:04 -0700

    part 2 started, deck of cards api connection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let baseURL = "http://numbersapi.com";
 
 let number = axios.get(`${baseURL}/${favNum}?json`);
 
+// Part 1
 // 1.
 number
     .then(data => console.log(data.data.text))
@@ -27,4 +28,21 @@ Promise.all(fourPromises)
     .then(each => (
         each.forEach(n => console.log(n.data.text))
     ))
+    .catch(err => console.log(err));
+
+// Part 2
+// 1. request a single card and console.log value and suit
+// get deck
+let deckURL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+let newDeck = axios.get(deckURL);
+newDeck
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+let deck_id = "degjyfk3jx0x";
+
+let drawCardURL = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
+let card = axios.get(drawCardURL);
+card
+    .then(data => console.log(data.data.cards[0].value, data.data.cards[0].suit))
     .catch(err => console.log(err));
